09470679575e1efafa7d372e52147eb9e160e5ec
Mihael Stormrage <mihan.msn@hotmail.com>
2020-09-27 11:08:33 +0700

    Place view scratch

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import request from 'request';
 import bodyParser from 'body-parser';
 import express from 'express';
 import exphbs from 'express-handlebars';
+// import {PythonShell} from 'python-shell';
+
 const app = express();
 
 const __dirname = path.resolve();
@@ -13,27 +15,45 @@ app.engine('.hbs', exphbs({
   extname: '.hbs',
   layoutsDir: path.join(__dirname, 'views/layouts')
 }));
+app.use(express.static('views/images'));
 
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.listen(3000);
 
-app.use('/form_handler', bodyParser.urlencoded({ extended: false }));
+app.use('/form_handler', bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.post('/form_handler', function(req, res, next) {
+app.post('/form_handler', function (req, res, next) {
+
+  const place = {
+    floor: 0,
+    room: 0,
+    stand: 0,
+    shelf: 0
+  };
+
   // Объект req.body содержит данные из переданной формы
   request.post({
     url: apiUrl,
     json: true,
-    form: JSON.stringify(req.body)
+    body: req.body
   }, (err, response, body) => {
-    if(err)
+    if (err)
       return res.status(500).send({message: err});
 
     console.log(body);
+    res.render('place', {
+      placePic: "f1ga2343h4bc3.png"
+    });
+    // console.log(JSON.parse(body.toString()).maxLastResult);
     // return res.send(body);
+    // return response;
   });
+
+  // const shell = new PythonShell('main.py', { mode: 'json'});
+  // shell.send(place);
+
 });
 
 app.get('/', (request, response) => {
